f93cdb6c3e659113ff4401d2b337d03b7ab3db6a
hoa0217 <gjwjdghk123@naver.com>
2023-05-14 20:18:45 +0900

    first commit

---
+++ src/main/java/com/modoospace/ModoospaceApplication.java
@@ -0,0 +1,13 @@
+package com.modoospace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModoospaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModoospaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/modoospace/ModoospaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modoospace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModoospaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
