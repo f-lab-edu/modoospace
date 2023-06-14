fbb42eea2c46e47780e412a90e6913ca54546739
hoa0217 <gjwjdghk123@naver.com>
2023-06-15 01:52:53 +0900

    chore: 테스트 space insert 쿼리 생성
    Related: #5

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,3 @@
 insert into member(CREATED_TIME, UPDATED_TIME, EMAIL, NAME, ROLE) values (now(), now(), 'gjwjdghk123@gmail.com', 'Jeonghwa Heo', 'ADMIN');
 insert into member(CREATED_TIME, UPDATED_TIME, EMAIL, NAME, ROLE) values (now(), now(), 'wjdghkwhdl@jr.naver.com', '허정화', 'HOST');
+insert into space(CREATED_TIME, UPDATED_TIME, DEPTH_FIRST, DEPTH_SECOND, DEPTH_THIRD, FULL_ADDRESS, NAME, HOST_ID) values (now(), now(), '서울', '강남구', '역삼동', '서울 강남구 역삼동 817-36 5층', '에프랩', 2);
