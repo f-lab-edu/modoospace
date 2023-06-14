10027857c460545566c8d0603b735291e31fc537
hoa0217 <gjwjdghk123@naver.com>
2023-06-14 22:23:38 +0900

    chore: Member 샘플데이터 추가
    Related: #5

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+insert into member(CREATED_TIME, UPDATED_TIME, EMAIL, NAME, ROLE) values (now(), now(), 'gjwjdghk123@gmail.com', 'Jeonghwa Heo', 'ADMIN');
+insert into member(CREATED_TIME, UPDATED_TIME, EMAIL, NAME, ROLE) values (now(), now(), 'wjdghkwhdl@jr.naver.com', '허정화', 'HOST');
