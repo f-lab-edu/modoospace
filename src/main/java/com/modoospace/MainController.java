94dd2a48292a448808707652e65f27836e8d5233
hoa0217 <gjwjdghk123@naver.com>
2023-06-15 03:17:56 +0900

    fix: 삭제한 클래스 import 제거
    Related: #5

--- src/main/java/com/modoospace/MainController.java
+++ src/main/java/com/modoospace/MainController.java
@@ -1,8 +1,6 @@
 package com.modoospace;
 
 import com.modoospace.config.auth.LoginEmail;
-import com.modoospace.config.auth.LoginUser;
-import com.modoospace.config.auth.dto.SessionMember;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
