b3fb59f4f7d8edf895a89c7d027aba8b011d65ed
hoa0217 <gjwjdghk123@naver.com>
2023-06-18 18:20:11 +0900

    refactor: 항목추가에 따른 시설 Dto 수정
    Related: #8

--- src/main/java/com/modoospace/space/controller/dto/FacilityReadDto.java
+++ src/main/java/com/modoospace/space/controller/dto/FacilityReadDto.java
@@ -1,7 +1,7 @@
 package com.modoospace.space.controller.dto;
 
-import com.modoospace.space.domain.Category;
 import com.modoospace.space.domain.Facility;
+import com.modoospace.space.domain.FacilityType;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.validation.constraints.NotEmpty;
@@ -21,24 +21,28 @@ public class FacilityReadDto {
   private String name;
 
   @NotNull
-  private Category category;
+  private FacilityType facilityType;
+
+  private String desc;
 
   @Builder
-  public FacilityReadDto(Long id, String name, Category category) {
+  public FacilityReadDto(Long id, String name, FacilityType facilityType, String desc) {
     this.id = id;
     this.name = name;
-    this.category = category;
+    this.facilityType = facilityType;
+    this.desc = desc;
   }
 
   public static FacilityReadDto toDto(Facility facility) {
     return FacilityReadDto.builder()
         .id(facility.getId())
         .name(facility.getName())
-        .category(facility.getCategory())
+        .facilityType(facility.getFacilityType())
+        .desc(facility.getDesc())
         .build();
   }
 
-  public static List<FacilityReadDto> toDtoList(List<Facility> facilities) {
+  public static List<FacilityReadDto> toDtos(List<Facility> facilities) {
     return facilities.stream()
         .map(FacilityReadDto::toDto)
         .collect(Collectors.toList());
