21bab8b3bae3b99ed2ad87d419fa294bf6b9f318
hoa0217 <gjwjdghk123@naver.com>
2023-06-18 18:25:35 +0900

    refactor: 항목 추가 및 기능 추가에 따른 공간 테스트 리팩토링
    - 공간은 Category를 갖는다.
    - 관리자도 다른 호스트의 공간을 생성할 수 있다.
    
    Related: #8

--- src/test/java/com/modoospace/space/sevice/SpaceServiceTest.java
+++ src/test/java/com/modoospace/space/sevice/SpaceServiceTest.java
@@ -13,6 +13,8 @@ import com.modoospace.space.controller.dto.SpaceCreateDto;
 import com.modoospace.space.controller.dto.SpaceReadDto;
 import com.modoospace.space.controller.dto.SpaceUpdateDto;
 import com.modoospace.space.domain.Address;
+import com.modoospace.space.domain.Category;
+import com.modoospace.space.domain.CategoryRepository;
 import com.modoospace.space.domain.SpaceRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -33,14 +35,18 @@ class SpaceServiceTest {
   @Autowired
   private SpaceRepository spaceRepository;
 
+  @Autowired
+  private CategoryRepository categoryRepository;
+
   private Member hostMember;
   private Member visitorMember;
   private Member adminMember;
   private Address address;
+  private Category category;
 
   @BeforeEach
   public void setUp() {
-    spaceService = new SpaceService(memberRepository, spaceRepository);
+    spaceService = new SpaceService(memberRepository, spaceRepository, categoryRepository);
 
     hostMember = Member.builder()
         .email("host@email")
@@ -70,14 +76,22 @@ class SpaceServiceTest {
         .depthThird("depthThird")
         .detailAddress("detailAddress")
         .build();
+
+    category = Category.builder()
+        .name("스터디 공간")
+        .build();
+
+    categoryRepository.save(category);
   }
 
-  @DisplayName("호스트일 경우 공간을 등록할 수 있다.")
+  @DisplayName("로그인한 멤버가 호스트일 경우 본인의 공간을 등록할 수 있다.")
   @Test
-  public void createSpace() {
+  public void createOwnSpace_IfHost() {
     SpaceCreateDto createDto = SpaceCreateDto.builder()
         .name("공간이름")
         .address(address)
+        .categoryId(category.getId())
+        .hostEmail(hostMember.getEmail())
         .build();
 
     Long spaceId = spaceService.createSpace(createDto, hostMember.getEmail());
@@ -86,30 +100,66 @@ class SpaceServiceTest {
     assertThat(retSpaceDto.getId()).isEqualTo(spaceId);
   }
 
-  @DisplayName("Host가 아닐 경우 공간을 등록 시 예외를 던진다.")
-  @Test
-  public void createSpace_throwException_IfVisitorMember() {
+  @DisplayName("로그인한 멤버가 호스트가 아닐 경우 본인 공간 등록 시 예외를 던진다.")
+  @ParameterizedTest
+  @ValueSource(strings = {"admin@email", "visitor@email"})
+  public void createOwnSpace_throwException_IfNotHost(String email) {
     SpaceCreateDto createDto = SpaceCreateDto.builder()
         .name("공간이름")
         .address(address)
+        .categoryId(category.getId())
+        .hostEmail(email)
         .build();
 
-    assertAll(
-        () -> assertThatThrownBy(() -> spaceService.createSpace(createDto, "notMember@Email"))
-            .isInstanceOf(NotFoundEntityException.class),
-        () -> assertThatThrownBy(
-            () -> spaceService.createSpace(createDto, visitorMember.getEmail()))
-            .isInstanceOf(PermissionDeniedException.class)
-    );
+    assertThatThrownBy(() -> spaceService.createSpace(createDto, email))
+        .isInstanceOf(PermissionDeniedException.class);
   }
 
-  @DisplayName("공간의 주인 또는 관리자만이 공간을 수정할 수 있다.")
-  @ParameterizedTest
-  @ValueSource(strings = {"host@email", "admin@email"})
-  public void updateSpace(String testEmail) {
+  @DisplayName("로그인한 멤버가 관리자일 경우 다른 호스트의 공간을 등록할 수 있다.")
+  @Test
+  public void createOtherSpace_IfAdmin() {
     SpaceCreateDto createDto = SpaceCreateDto.builder()
         .name("공간이름")
         .address(address)
+        .categoryId(category.getId())
+        .hostEmail(hostMember.getEmail())
+        .build();
+
+    Long spaceId = spaceService.createSpace(createDto, adminMember.getEmail());
+
+    SpaceReadDto retSpaceDto = spaceService.findSpace(spaceId);
+    assertThat(retSpaceDto.getId()).isEqualTo(spaceId);
+  }
+
+  @DisplayName("로그인한 멤버가 호스트일지라도 본인 공간을 생성하는게 아니라면 예외를 던진다.")
+  @Test
+  public void createOtherSpace_throwException_IfNotAdmin() {
+    Member hostMember2 = Member.builder()
+        .email("host2@email")
+        .name("host2")
+        .role(Role.HOST)
+        .build();
+    memberRepository.save(hostMember2);
+    SpaceCreateDto createDto = SpaceCreateDto.builder()
+        .name("공간이름")
+        .address(address)
+        .categoryId(category.getId())
+        .hostEmail(hostMember2.getEmail())
+        .build();
+
+    assertThatThrownBy(() -> spaceService.createSpace(createDto, hostMember.getEmail()))
+        .isInstanceOf(PermissionDeniedException.class);
+  }
+
+  @DisplayName("공간의 주인/관리자만이 공간을 수정할 수 있다.")
+  @ParameterizedTest
+  @ValueSource(strings = {"host@email", "admin@email"})
+  public void updateSpace(String email) {
+    SpaceCreateDto createDto = SpaceCreateDto.builder()
+        .name("공간이름")
+        .address(address)
+        .categoryId(category.getId())
+        .hostEmail(hostMember.getEmail())
         .build();
     Long spaceId = spaceService.createSpace(createDto, hostMember.getEmail());
     Address updateAddress = Address.builder()
@@ -122,27 +172,31 @@ class SpaceServiceTest {
         .id(spaceId)
         .name("업데이트공간")
         .address(updateAddress)
+        .categoryId(category.getId())
         .build();
 
-    spaceService.updateSpace(updateDto, testEmail);
+    spaceService.updateSpace(updateDto, email);
 
     SpaceReadDto retSpaceDto = spaceService.findSpace(spaceId);
     assertThat(retSpaceDto.getName()).isEqualTo("업데이트공간");
     assertThat(retSpaceDto.getAddress()).isEqualTo(updateAddress);
   }
 
-  @DisplayName("공간의 주인 또는 관리자가 아닐 경우 공간을 수정 시 예외를 던진다.")
+  @DisplayName("공간의 주인/관리자가 아닐 경우 공간을 수정 시 예외를 던진다.")
   @Test
   public void updateSpace_throwException_IfAdminMemberOrOwnSpace() {
     SpaceCreateDto createDto = SpaceCreateDto.builder()
         .name("공간이름")
         .address(address)
+        .categoryId(category.getId())
+        .hostEmail(hostMember.getEmail())
         .build();
     Long spaceId = spaceService.createSpace(createDto, hostMember.getEmail());
     SpaceUpdateDto updateDto = SpaceUpdateDto.builder()
         .id(spaceId)
         .name("업데이트")
         .address(address)
+        .categoryId(category.getId())
         .build();
 
     assertAll(
@@ -154,13 +208,15 @@ class SpaceServiceTest {
     );
   }
 
-  @DisplayName("공간의 주인 또는 관리자만이 공간을 삭제할 수 있다.")
+  @DisplayName("공간의 주인/관리자만이 공간을 삭제할 수 있다.")
   @ParameterizedTest
   @ValueSource(strings = {"host@email", "admin@email"})
   public void deleteSpace(String testEmail) {
     SpaceCreateDto createDto = SpaceCreateDto.builder()
         .name("공간이름")
         .address(address)
+        .categoryId(category.getId())
+        .hostEmail(hostMember.getEmail())
         .build();
     Long spaceId = spaceService.createSpace(createDto, hostMember.getEmail());
 
@@ -175,6 +231,8 @@ class SpaceServiceTest {
     SpaceCreateDto createDto = SpaceCreateDto.builder()
         .name("공간이름")
         .address(address)
+        .categoryId(category.getId())
+        .hostEmail(hostMember.getEmail())
         .build();
     Long spaceId = spaceService.createSpace(createDto, hostMember.getEmail());
 
@@ -193,11 +251,15 @@ class SpaceServiceTest {
     SpaceCreateDto createDto = SpaceCreateDto.builder()
         .name("공간이름")
         .address(address)
+        .categoryId(category.getId())
+        .hostEmail(hostMember.getEmail())
         .build();
     spaceService.createSpace(createDto, hostMember.getEmail());
     createDto = SpaceCreateDto.builder()
         .name("공간이름2")
         .address(address)
+        .categoryId(category.getId())
+        .hostEmail(hostMember.getEmail())
         .build();
     spaceService.createSpace(createDto, hostMember.getEmail());
 
