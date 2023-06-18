75eed7ff28d5d86a21e2ce7835b88d56b7cfb5ce
hoa0217 <gjwjdghk123@naver.com>
2023-06-18 18:19:15 +0900

    feature: 시설 엔티티 설명, 시설유형, 생성/수정시간 추가
    Related: #8

--- src/main/java/com/modoospace/space/domain/Facility.java
+++ src/main/java/com/modoospace/space/domain/Facility.java
@@ -2,20 +2,24 @@ package com.modoospace.space.domain;
 
 import static javax.persistence.FetchType.LAZY;
 
+import com.modoospace.common.BaseTimeEntity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Facility {
+public class Facility extends BaseTimeEntity {
 
   @Id
   @GeneratedValue
@@ -25,11 +29,23 @@ public class Facility {
   @Column(nullable = false)
   private String name;
 
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private FacilityType facilityType;
+
+  private String desc;
+
   @ManyToOne(fetch = LAZY)
   @JoinColumn(name = "space_id")
   private Space space;
 
-  @ManyToOne(fetch = LAZY)
-  @JoinColumn(name = "category_id")
-  private Category category;
+  @Builder
+  public Facility(Long id, String name, FacilityType facilityType, String desc,
+      Space space) {
+    this.id = id;
+    this.name = name;
+    this.facilityType = facilityType;
+    this.desc = desc;
+    this.space = space;
+  }
 }
