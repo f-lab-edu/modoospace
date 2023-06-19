3aca79f777123adc8d6d02dd3e0bf091ca7a8f27
hoa0217 <gjwjdghk123@naver.com>
2023-06-19 23:41:51 +0900

    refactor: member api 리팩토링
    Related: #8

--- src/main/java/com/modoospace/member/controller/AdminMemberController.java
+++ src/main/java/com/modoospace/member/controller/AdminMemberController.java
@@ -13,12 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/admin")
+@RequestMapping("/api/v1/admin/members")
 public class AdminMemberController {
 
   private final MemberService memberService;
 
-  @PutMapping("/member")
+  @PutMapping
   public ResponseEntity<Void> updateMemberRole(@RequestBody @Valid MemberUpdateDto updateDto,
       @LoginEmail String loginEmail) {
     memberService.updateMemberRole(updateDto, loginEmail);
