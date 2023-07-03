23ab59b84eba072cbdfc5ff70558e5752b2987e4
haileykim2014 <haileykim2014@gmail.com>
2023-07-03 20:11:24 +0900

    feature/17-cors cors설정

--- src/main/java/com/modoospace/config/WebConfig.java
+++ src/main/java/com/modoospace/config/WebConfig.java
@@ -5,6 +5,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @RequiredArgsConstructor
@@ -16,5 +17,12 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
     resolvers.add(loginEmailArgumentResolver);
+  }
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedOrigins("*")
+        .allowedMethods("*");
   }
 }
