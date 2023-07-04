4926428503cce3b7afba1b01303bb63ecd5fa2c1
hoa0217 <gjwjdghk123@naver.com>
2023-07-05 03:13:07 +0900

    test: 스케줄데이터 isOpen 로직 수정
    - 시간범위가 분리된 case도 테스트
    
    Related: #7

--- src/test/java/com/modoospace/space/domain/FacilitySchedulesTest.java
+++ src/test/java/com/modoospace/space/domain/FacilitySchedulesTest.java
@@ -14,16 +14,16 @@ import org.junit.jupiter.api.Test;
 
 class FacilitySchedulesTest {
 
-  private LocalDate testDate;
+  private LocalDate nowDate;
 
   @BeforeEach
   public void setUp() {
-    testDate = LocalDate.now();
-    if (testDate.getDayOfWeek().equals(DayOfWeek.SUNDAY)) {
-      testDate = testDate.plusDays(1);
+    nowDate = LocalDate.now();
+    if (nowDate.getDayOfWeek().equals(DayOfWeek.SUNDAY)) {
+      nowDate = nowDate.plusDays(1);
     }
-    if (testDate.getDayOfWeek().equals(DayOfWeek.SATURDAY)) {
-      testDate = testDate.plusDays(2);
+    if (nowDate.getDayOfWeek().equals(DayOfWeek.SATURDAY)) {
+      nowDate = nowDate.plusDays(2);
     }
   }
 
@@ -69,12 +69,8 @@ class FacilitySchedulesTest {
   public void isOpen() {
     List<TimeSetting> timeSettings = Arrays.asList(
         TimeSetting.builder()
-            .startTime(LocalTime.of(9, 0, 0))
-            .endTime(LocalTime.of(11, 59, 59))
-            .build(),
-        TimeSetting.builder()
-            .startTime(LocalTime.of(13, 0, 0))
-            .endTime(LocalTime.of(17, 59, 59))
+            .startTime(LocalTime.of(0, 0, 0))
+            .endTime(LocalTime.of(23, 59, 59))
             .build());
     List<WeekdaySetting> weekdaySettings = Arrays.asList(
         WeekdaySetting.builder()
@@ -91,16 +87,71 @@ class FacilitySchedulesTest {
             .build(),
         WeekdaySetting.builder()
             .weekday(DayOfWeek.FRIDAY)
+            .build(),
+        WeekdaySetting.builder()
+            .weekday(DayOfWeek.SATURDAY)
+            .build(),
+        WeekdaySetting.builder()
+            .weekday(DayOfWeek.SUNDAY)
             .build());
     FacilitySchedules facilitySchedules = FacilitySchedules
         .createFacilitySchedules(new TimeSettings(timeSettings),
             new WeekdaySettings(weekdaySettings));
 
-    LocalDateTime startDateTime = LocalDateTime.of(testDate, LocalTime.of(9, 0));
-    LocalDateTime endDateTime = LocalDateTime.of(testDate, LocalTime.of(11, 59, 59));
+    LocalDateTime startDateTime = LocalDateTime.of(nowDate, LocalTime.of(9, 0, 0));
+    LocalDateTime endDateTime = LocalDateTime.of(nowDate, LocalTime.of(11, 59, 59));
     assertThat(facilitySchedules.isOpen(startDateTime, endDateTime)).isTrue();
 
-    endDateTime = LocalDateTime.of(testDate, LocalTime.of(18, 0, 0));
+    startDateTime = LocalDateTime.of(nowDate, LocalTime.of(0, 0, 0));
+    endDateTime = LocalDateTime.of(nowDate.plusDays(6), LocalTime.of(23, 59, 59));
+    assertThat(facilitySchedules.isOpen(startDateTime, endDateTime)).isTrue();
+  }
+
+  @DisplayName("해당 시간범위에 시설이 Open했는지 여부를 반환한다. (날짜의 시간범위가 분리된 case 테스트)")
+  @Test
+  public void isOpen_ifWeekdaySettingUpdate() {
+    List<TimeSetting> timeSettings = Arrays.asList(
+        TimeSetting.builder()
+            .startTime(LocalTime.of(0, 0, 0))
+            .endTime(LocalTime.of(23, 59, 59))
+            .build());
+    List<WeekdaySetting> weekdaySettings = Arrays.asList(
+        WeekdaySetting.builder()
+            .weekday(DayOfWeek.MONDAY)
+            .build(),
+        WeekdaySetting.builder()
+            .weekday(DayOfWeek.TUESDAY)
+            .build(),
+        WeekdaySetting.builder()
+            .weekday(DayOfWeek.WEDNESDAY)
+            .build(),
+        WeekdaySetting.builder()
+            .weekday(DayOfWeek.THURSDAY)
+            .build(),
+        WeekdaySetting.builder()
+            .weekday(DayOfWeek.FRIDAY)
+            .build(),
+        WeekdaySetting.builder()
+            .weekday(DayOfWeek.SATURDAY)
+            .build(),
+        WeekdaySetting.builder()
+            .weekday(DayOfWeek.SUNDAY)
+            .build());
+    FacilitySchedules facilitySchedules = FacilitySchedules
+        .createFacilitySchedules(new TimeSettings(timeSettings),
+            new WeekdaySettings(weekdaySettings));
+    FacilitySchedule updateFacilitySchedule = FacilitySchedule.builder()
+        .startDateTime(LocalDateTime.of(nowDate.plusDays(1), LocalTime.of(0, 0, 0)))
+        .endDateTime(LocalDateTime.of(nowDate.plusDays(1), LocalTime.of(17, 59, 59)))
+        .build(); // 오늘날짜 + 1 스케줄 데이터를 업데이트한다. (0시 ~ 18시로 변경)
+    FacilitySchedule facilitySchedule = facilitySchedules.getFacilitySchedules().get(nowDate.getDayOfMonth());
+    facilitySchedule.update(updateFacilitySchedule);
+
+    LocalDateTime startDateTime = LocalDateTime.of(nowDate, LocalTime.of(9, 0, 0));
+    LocalDateTime endDateTime = LocalDateTime.of(nowDate.plusDays(1), LocalTime.of(17, 59, 59));
+    assertThat(facilitySchedules.isOpen(startDateTime, endDateTime)).isTrue();
+
+    endDateTime = LocalDateTime.of(nowDate.plusDays(2), LocalTime.of(17, 59, 59));
     assertThat(facilitySchedules.isOpen(startDateTime, endDateTime)).isFalse();
   }
 }
