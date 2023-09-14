d6bb7c527f7e74a2b97634ad376ac844d36f9c0b
hoa0217 <gjwjdghk123@naver.com>
2023-09-14 12:34:22 +0900

    test: redis 캐시 데이터를 변경하는 테스트는 수행 후 flushall을 수행한다.
    Related: #36

--- src/test/java/com/modoospace/member/repository/MemberCacheRepositoryTest.java
+++ src/test/java/com/modoospace/member/repository/MemberCacheRepositoryTest.java
@@ -4,11 +4,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.modoospace.member.domain.Member;
 import com.modoospace.member.domain.Role;
-import java.util.Optional;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
@@ -17,6 +18,14 @@ class MemberCacheRepositoryTest {
 
   @Autowired
   MemberCacheRepository memberCacheRepository;
+
+  @Autowired
+  StringRedisTemplate redisTemplate;
+
+  @AfterEach
+  public void after() {
+    redisTemplate.getConnectionFactory().getConnection().flushAll();
+  }
 
   @DisplayName("Redis에 Member를 캐싱하고 가져온다.")
   @Test
