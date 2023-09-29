67b0f1af9a8d78957d44f72773afd5cc4864a7c4
hoa0217 <gjwjdghk123@naver.com>
2023-09-30 01:51:32 +0900

    fix: SpELParser key pattern 수정 및 RedisTemplate ➡️ StringRedisTemplate
    Related: #36

--- src/main/java/com/modoospace/config/redis/CachePrefixEvictAdvisor.java
+++ src/main/java/com/modoospace/config/redis/CachePrefixEvictAdvisor.java
@@ -2,33 +2,34 @@ package com.modoospace.config.redis;
 
 import com.modoospace.common.SpELParser.CustomSpELParser;
 import java.util.Set;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Slf4j
 @Component
-@RequiredArgsConstructor
 public class CachePrefixEvictAdvisor {
 
-  private final RedisTemplate redisTemplate;
+  @Autowired
+  StringRedisTemplate redisTemplate;
 
   @Around("@annotation(com.modoospace.config.redis.CachePrefixEvict)")
   public Object processCachePrefixEvict(ProceedingJoinPoint joinPoint) throws Throwable {
     MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
     CachePrefixEvict annotation = methodSignature.getMethod().getAnnotation(CachePrefixEvict.class);
-
-    String key = CustomSpELParser.extractValueFromExpression(
+    Object prefix = CustomSpELParser.extractValueFromExpression(
         methodSignature.getParameterNames(), joinPoint.getArgs(), annotation.key()
-    ).toString();
-    log.info("cacheNames = {}, key = {} redis evict try", annotation.cacheNames(), key);
-    Set<String> keys = redisTemplate.keys(annotation.key() + "*");
+    );
+
+    String pattern = String.format("%s::%s:*", annotation.cacheNames(), prefix);
+    log.info("key pattern ({}) evict try in redis", pattern);
+    Set<String> keys = redisTemplate.keys(pattern);
     if (keys != null) {
       log.info("{} clear complete", keys);
       redisTemplate.delete(keys);
--- src/test/java/com/modoospace/alarm/service/AlarmServiceTest.java
+++ src/test/java/com/modoospace/alarm/service/AlarmServiceTest.java
@@ -3,18 +3,18 @@ package com.modoospace.alarm.service;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.modoospace.alarm.controller.dto.AlarmEvent;
-import com.modoospace.alarm.controller.dto.AlarmReadDto;
+import com.modoospace.alarm.domain.AlarmRepository;
 import com.modoospace.alarm.domain.AlarmType;
 import com.modoospace.member.domain.Member;
 import com.modoospace.member.domain.MemberRepository;
 import com.modoospace.member.domain.Role;
 import java.util.Set;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.ActiveProfiles;
@@ -32,6 +32,9 @@ class AlarmServiceTest {
   private MemberRepository memberRepository;
 
   @Autowired
+  private AlarmRepository alarmRepository;
+
+  @Autowired
   private StringRedisTemplate redisTemplate;
 
   private Member hostMember;
@@ -45,21 +48,50 @@ class AlarmServiceTest {
         .build();
 
     memberRepository.save(hostMember);
+
+    for (int i = 0; i < 10; i++) {
+      AlarmEvent testEvent = AlarmEvent.builder()
+          .memberId(hostMember.getId())
+          .reservationId((long) i)
+          .facilityName("테스트 시설")
+          .alarmType(AlarmType.NEW_RESERVATION)
+          .build();
+      alarmRepository.save(testEvent.toEntity());
+    }
+  }
+
+  @AfterEach
+  public void after() {
+    redisTemplate.getConnectionFactory().getConnection().flushAll();
+  }
+
+  @DisplayName("알람을 검색하면, searchAlarms::멤버ID:페이지넘버를 키값으로 결과가 캐싱된다.")
+  @Test
+  public void searchAlarms_redisSave() {
+    PageRequest pageRequest = PageRequest.of(0, 10);
+    alarmService.searchAlarms("host@email", pageRequest);
+
+    String pattern = "searchAlarms::" + hostMember.getId() + ":*";
+    Set<String> keys = redisTemplate.keys(pattern);
+    assertThat(keys).hasSize(1);
+    System.out.println("keys = " + keys);
   }
 
   @DisplayName("알람을 새로 save하면 redis에 저장되어있던 해당 멤버의 알람 페이징 데이터가 전부 삭제된다.")
   @Test
   public void saveAndSend_EmptyRedisKeys() {
+    PageRequest pageRequest = PageRequest.of(0, 10);
+    alarmService.searchAlarms("host@email", pageRequest);
     AlarmEvent testEvent = AlarmEvent.builder()
         .memberId(hostMember.getId())
         .reservationId(1L)
         .facilityName("테스트 시설")
         .alarmType(AlarmType.NEW_RESERVATION)
         .build();
-
     alarmService.saveAndSend(testEvent);
 
-    Set<String> keys = redisTemplate.keys(testEvent.getMemberId() + ":*");
+    String pattern = "searchAlarms::" + hostMember.getId() + ":*";
+    Set<String> keys = redisTemplate.keys(pattern);
     assertThat(keys).isEmpty();
   }
 }
