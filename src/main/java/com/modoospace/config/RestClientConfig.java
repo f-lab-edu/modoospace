d826178c36a7792d5c074544fe4471f69d624c9d
hoa0217 <gjwjdghk123@naver.com>
2024-02-15 18:51:27 +0900

    chore: elastic search 설정
    Related: #32

---
+++ src/main/java/com/modoospace/config/RestClientConfig.java
@@ -0,0 +1,26 @@
+package com.modoospace.config;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+
+@Configuration
+public class RestClientConfig extends AbstractElasticsearchConfiguration {
+
+    @Value("${spring.elasticsearch.host}")
+    private String host;
+    @Value("${spring.elasticsearch.port}")
+    private int port;
+
+    @Override
+    public RestHighLevelClient elasticsearchClient() {
+        final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+            .connectedTo(host + ":" + port)
+            .build();
+
+        return RestClients.create(clientConfiguration).rest();
+    }
+}
---
+++ src/main/java/com/modoospace/space/domain/SpaceIndex.java
@@ -0,0 +1,42 @@
+package com.modoospace.space.domain;
+
+import javax.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+@Document(indexName = "space")
+@Getter
+@NoArgsConstructor
+public class SpaceIndex {
+
+    @Id
+    private Long id;
+    private String name;
+    private String description;
+    private String categoryName;
+    private String address;
+
+    @Builder
+    public SpaceIndex(Long id, String name, String description, String categoryName,
+        String address) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.categoryName = categoryName;
+        this.address = address;
+    }
+
+    public static SpaceIndex of(Space space) {
+        return SpaceIndex.builder()
+            .id(space.getId())
+            .name(space.getName())
+            .description(space.getDescription())
+            .categoryName(space.getCategory().getName())
+            .address(space.getAddress().getDepthFirst() + " "
+                + space.getAddress().getDepthSecond() + " "
+                + space.getAddress().getDepthThird())
+            .build();
+    }
+}
