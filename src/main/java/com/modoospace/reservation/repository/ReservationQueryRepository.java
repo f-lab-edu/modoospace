a737ed843b291e3760075a1ca7584d14500f089c
hoa0217 <gjwjdghk123@naver.com>
2024-03-04 00:18:07 +0900

    refactor: ReservationQueryRepository
    의미없는 동적쿼리 제거 및 testcase 추가
    
    Related: #43

--- src/main/java/com/modoospace/reservation/repository/ReservationQueryRepository.java
+++ src/main/java/com/modoospace/reservation/repository/ReservationQueryRepository.java
@@ -1,18 +1,17 @@
 package com.modoospace.reservation.repository;
 
-import static com.modoospace.reservation.domain.QReservation.reservation;
-
 import com.modoospace.reservation.domain.DateTimeRange;
 import com.modoospace.reservation.domain.Reservation;
 import com.modoospace.reservation.domain.ReservationStatus;
 import com.modoospace.space.domain.Facility;
-import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static com.modoospace.reservation.domain.QReservation.reservation;
 
 @RequiredArgsConstructor
 @Repository
@@ -28,31 +27,15 @@ public class ReservationQueryRepository {
         return !findConflictingReservation(facility, dateTimeRange).isEmpty();
     }
 
-    private List<Reservation> findConflictingReservation(Facility facility,
-        DateTimeRange dateTimeRange) {
+    private List<Reservation> findConflictingReservation(Facility facility, DateTimeRange dateTimeRange) {
 
         return jpaQueryFactory
-            .selectFrom(reservation)
-            .where(facilityEq(facility),
-                statusNe(ReservationStatus.CANCELED),
-                startDateTimeBefore(dateTimeRange.getEndDateTime()),
-                endDateTimeAfter(dateTimeRange.getStartDateTime()))
-            .fetch();
-    }
-
-    private BooleanExpression facilityEq(Facility facility) {
-        return facility != null ? reservation.facility.eq(facility) : null;
-    }
-
-    private BooleanExpression statusNe(ReservationStatus status) {
-        return status != null ? reservation.status.ne(status) : null;
-    }
-
-    private BooleanExpression startDateTimeBefore(LocalDateTime dateTime) {
-        return dateTime != null ? reservation.dateTimeRange.startDateTime.before(dateTime) : null;
-    }
-
-    private BooleanExpression endDateTimeAfter(LocalDateTime dateTime) {
-        return dateTime != null ? reservation.dateTimeRange.endDateTime.after(dateTime) : null;
+                .selectFrom(reservation)
+                .where(reservation.facility.eq(facility),
+                        reservation.status.ne(ReservationStatus.CANCELED),
+                        reservation.dateTimeRange.startDateTime.before(dateTimeRange.getEndDateTime()),
+                        reservation.dateTimeRange.endDateTime.after(dateTimeRange.getStartDateTime())
+                )
+                .fetch();
     }
 }
