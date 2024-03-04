b5c4864601baa6c3e12cccee41236334233e4276
hoa0217 <gjwjdghk123@naver.com>
2024-03-05 00:11:27 +0900

    refactor: test 메세지 알림 api 추가
    Related: #43

--- src/main/java/com/modoospace/alarm/controller/AlarmController.java
+++ src/main/java/com/modoospace/alarm/controller/AlarmController.java
@@ -1,17 +1,14 @@
 package com.modoospace.alarm.controller;
 
 import com.modoospace.alarm.controller.dto.AlarmResponse;
+import com.modoospace.alarm.domain.AlarmType;
 import com.modoospace.alarm.service.AlarmService;
 import com.modoospace.config.auth.LoginEmail;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RequiredArgsConstructor
@@ -19,24 +16,30 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 @RequestMapping("/api/v1/alarms")
 public class AlarmController {
 
-  private final AlarmService alarmService;
+    private final AlarmService alarmService;
 
-  @GetMapping()
-  public ResponseEntity<Page<AlarmResponse>> search(@LoginEmail String loginEmail,
-      Pageable pageable) {
-    Page<AlarmResponse> alarms = alarmService.searchAlarms(loginEmail, pageable);
-    return ResponseEntity.ok().body(alarms);
-  }
+    @GetMapping()
+    public ResponseEntity<Page<AlarmResponse>> search(@LoginEmail String loginEmail,
+                                                      Pageable pageable) {
+        Page<AlarmResponse> alarms = alarmService.searchAlarms(loginEmail, pageable);
+        return ResponseEntity.ok().body(alarms);
+    }
 
-  @DeleteMapping("/{alarmId}")
-  public ResponseEntity<Void> delete(@PathVariable Long alarmId,
-      @LoginEmail String loginEmail) {
-    alarmService.delete(alarmId, loginEmail);
-    return ResponseEntity.noContent().build();
-  }
+    @DeleteMapping("/{alarmId}")
+    public ResponseEntity<Void> delete(@PathVariable Long alarmId,
+                                       @LoginEmail String loginEmail) {
+        alarmService.delete(alarmId, loginEmail);
+        return ResponseEntity.noContent().build();
+    }
 
-  @GetMapping("/subscribe")
-  public SseEmitter subscribe(@LoginEmail String loginEmail) {
-    return alarmService.connectAlarm(loginEmail);
-  }
+    @GetMapping(value = "/subscribe", produces = "text/event-stream")
+    public ResponseEntity<SseEmitter> subscribe(@LoginEmail String loginEmail) {
+        return ResponseEntity.ok(alarmService.connectAlarm(loginEmail));
+    }
+
+    @PostMapping(value = "/send/{email}")
+    public ResponseEntity<Void> send(@PathVariable String email) {
+        alarmService.send(email, new AlarmResponse(null, null, "테스트시설", AlarmType.NEW_RESERVATION));
+        return ResponseEntity.noContent().build();
+    }
 }
