1263282e5f95b7b6b2f0ba86725e8a482c499dbf
hoa0217 <gjwjdghk123@naver.com>
2024-03-05 00:12:51 +0900

    chore: security 설정 추가(테스트 알람전송 api 모든 사용자 허용)
    Related: #43

--- src/main/java/com/modoospace/config/auth/SecurityConfiguration.java
+++ src/main/java/com/modoospace/config/auth/SecurityConfiguration.java
@@ -12,28 +12,29 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class SecurityConfiguration {
 
-  private final CustomOAuth2UserService customOAuth2UserService;
+    private final CustomOAuth2UserService customOAuth2UserService;
 
-  @Bean
-  protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.csrf().disable().cors().disable()
-        .headers().frameOptions().disable() // h2-console화면을 사용하기 위해 해당 옵션들을 disable
-        .and()
-        .authorizeHttpRequests(request -> request
-                .antMatchers(HttpMethod.GET, "/", "/error", "/api/v1/spaces/**",
-                    "/api/v1/spaces/*/facilities/**",
-                    "/api/v1/facilities/*/schedules/**",
-                    "/api/v1/visitors/reservations/facilities/*/availability/**").permitAll()
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable().cors().disable()
+                .headers().frameOptions().disable() // h2-console화면을 사용하기 위해 해당 옵션들을 disable
+                .and()
+                .authorizeHttpRequests(request -> request
+                                .antMatchers(HttpMethod.GET, "/", "/error", "/api/v1/spaces/**",
+                                        "/api/v1/spaces/*/facilities/**",
+                                        "/api/v1/facilities/*/schedules/**",
+                                        "/api/v1/visitors/reservations/facilities/*/availability/**").permitAll()
+                                .antMatchers(HttpMethod.POST, "/api/v1/alarms/send/**").permitAll()
 //            .antMatchers(HttpMethod.POST, "/api/v1/space").hasRole(Role.HOST.name())
-                .antMatchers("/api/v1/admin/**").hasRole(Role.ADMIN.name())
-                .anyRequest().authenticated()
-        )
-        .oauth2Login().userInfoEndpoint()
-        .userService(customOAuth2UserService); // 로그인 성공 후 후속조치를 진행할 UserServie 인터페이스의 구현체 등록
+                                .antMatchers("/api/v1/admin/**").hasRole(Role.ADMIN.name())
+                                .anyRequest().authenticated()
+                )
+                .oauth2Login().userInfoEndpoint()
+                .userService(customOAuth2UserService); // 로그인 성공 후 후속조치를 진행할 UserServie 인터페이스의 구현체 등록
 
-    http.logout()
-        .logoutSuccessUrl("/")
-        .invalidateHttpSession(true); // 세션 날리기
-    return http.build();
-  }
+        http.logout()
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true); // 세션 날리기
+        return http.build();
+    }
 }
