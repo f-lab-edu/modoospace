5af787b4b34a04de816ab83e82c1b3be8fc7f027
hoa0217 <gjwjdghk123@naver.com>
2024-03-07 01:54:14 +0900

    refactor: GlobalExceptionHandler 정리
    - 추가된 Exception handle 메서드 추가

--- src/main/java/com/modoospace/common/GlobalExceptionHandler.java
+++ src/main/java/com/modoospace/common/GlobalExceptionHandler.java
@@ -1,14 +1,6 @@
 package com.modoospace.common;
 
-import com.modoospace.common.exception.AlarmSendException;
-import com.modoospace.common.exception.ConflictingReservationException;
-import com.modoospace.common.exception.ConflictingTimeException;
-import com.modoospace.common.exception.DuplicatedWeekdayException;
-import com.modoospace.common.exception.InvalidTimeRangeException;
-import com.modoospace.common.exception.NotFoundEntityException;
-import com.modoospace.common.exception.NotOpenedFacilityException;
-import com.modoospace.common.exception.PermissionDeniedException;
-import com.modoospace.common.exception.SSEConnectError;
+import com.modoospace.common.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,57 +9,68 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-  @ExceptionHandler(AlarmSendException.class)
-  public ResponseEntity<String> handleAlarmSendException(
-      InvalidTimeRangeException exception) {
-    return new ResponseEntity<>(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-  }
+    @ExceptionHandler(AlarmSendException.class)
+    public ResponseEntity<String> handleAlarmSendException(InvalidTimeRangeException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
-  @ExceptionHandler(ConflictingReservationException.class)
-  public ResponseEntity<String> handleConflictingReservationException(
-      ConflictingReservationException exception) {
-    return new ResponseEntity<>(exception.getMessage(), HttpStatus.CONFLICT);
-  }
+    @ExceptionHandler(SSEConnectError.class)
+    public ResponseEntity<String> handleSSEConnectError(SSEConnectError exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
-  @ExceptionHandler(ConflictingTimeException.class)
-  public ResponseEntity<String> handleConflictingTimeException(
-      ConflictingTimeException exception) {
-    return new ResponseEntity<>(exception.getMessage(), HttpStatus.CONFLICT);
-  }
+    @ExceptionHandler(RedisProcessingException.class)
+    public ResponseEntity<String> handleRedisProcessingException(SSEConnectError exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
-  @ExceptionHandler(DuplicatedWeekdayException.class)
-  public ResponseEntity<String> handleDuplicatedWeekdayException(
-      ConflictingTimeException exception) {
-    return new ResponseEntity<>(exception.getMessage(), HttpStatus.CONFLICT);
-  }
+    @ExceptionHandler(ConflictingReservationException.class)
+    public ResponseEntity<String> handleConflictingReservationException(ConflictingReservationException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.CONFLICT);
+    }
 
-  @ExceptionHandler(InvalidTimeRangeException.class)
-  public ResponseEntity<String> handleInvalidTimeRangeException(
-      InvalidTimeRangeException exception) {
-    return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
-  }
+    @ExceptionHandler(ConflictingTimeException.class)
+    public ResponseEntity<String> handleConflictingTimeException(ConflictingTimeException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.CONFLICT);
+    }
 
-  @ExceptionHandler(NotFoundEntityException.class)
-  public ResponseEntity<String> handleNotFoundEntityException(
-      NotFoundEntityException exception) {
-    return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
-  }
+    @ExceptionHandler(DuplicatedWeekdayException.class)
+    public ResponseEntity<String> handleDuplicatedWeekdayException(ConflictingTimeException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.CONFLICT);
+    }
 
-  @ExceptionHandler(NotOpenedFacilityException.class)
-  public ResponseEntity<String> handleNotOpenedFacilityException(
-      NotOpenedFacilityException exception) {
-    return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
-  }
+    @ExceptionHandler(PermissionDeniedException.class)
+    public ResponseEntity<String> handlePermissionDeniedException(PermissionDeniedException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.FORBIDDEN);
+    }
 
-  @ExceptionHandler(PermissionDeniedException.class)
-  public ResponseEntity<String> handlePermissionDeniedException(
-      PermissionDeniedException exception) {
-    return new ResponseEntity<>(exception.getMessage(), HttpStatus.FORBIDDEN);
-  }
+    @ExceptionHandler(NotFoundEntityException.class)
+    public ResponseEntity<String> handleNotFoundEntityException(NotFoundEntityException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
 
-  @ExceptionHandler(SSEConnectError.class)
-  public ResponseEntity<String> handleSSEConnectError(
-      SSEConnectError exception) {
-    return new ResponseEntity<>(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-  }
+    @ExceptionHandler(InvalidTimeRangeException.class)
+    public ResponseEntity<String> handleInvalidTimeRangeException(InvalidTimeRangeException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NotOpenedFacilityException.class)
+    public ResponseEntity<String> handleNotOpenedFacilityException(NotOpenedFacilityException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(DeleteSpaceWithFacilitiesException.class)
+    public ResponseEntity<String> handleDeleteSpaceWithFacilitiesException(DeleteSpaceWithFacilitiesException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InvalidNumOfUserException.class)
+    public ResponseEntity<String> handleInvalidNumOfUserException(InvalidNumOfUserException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(LimitNumOfUserException.class)
+    public ResponseEntity<String> handleLimitNumOfUserException(LimitNumOfUserException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 }
--- src/main/java/com/modoospace/common/exception/InvalidHourException.java
+++
@@ -1,8 +0,0 @@
-package com.modoospace.common.exception;
-
-public class InvalidHourException extends RuntimeException {
-
-    public InvalidHourException(Integer hour) {
-        super("올바른 시간이 아닙니다.(" + hour + ")");
-    }
-}
