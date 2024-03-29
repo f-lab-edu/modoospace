e05fad8a1a06d7021f3ff7f46dde694b47c4e75e
hoa0217 <gjwjdghk123@naver.com>
2024-03-29 14:32:54 +0900

    test: AlarmConsumer, AlarmProducer
    Related: #68

---
+++ src/test/java/com/modoospace/alarm/consumer/AlarmConsumerTest.java
@@ -0,0 +1,82 @@
+package com.modoospace.alarm.consumer;
+
+import com.modoospace.AbstractIntegrationContainerBaseTest;
+import com.modoospace.alarm.controller.dto.AlarmEvent;
+import com.modoospace.alarm.domain.Alarm;
+import com.modoospace.alarm.domain.AlarmRepository;
+import com.modoospace.alarm.domain.AlarmType;
+import com.modoospace.common.exception.NotFoundEntityException;
+import com.modoospace.member.domain.Member;
+import com.modoospace.member.domain.MemberRepository;
+import com.modoospace.member.domain.Role;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testcontainers.shaded.com.fasterxml.jackson.core.JsonProcessingException;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class AlarmConsumerTest extends AbstractIntegrationContainerBaseTest {
+
+    @Autowired
+    AlarmConsumer alarmConsumer;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    AlarmRepository alarmRepository;
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @AfterEach
+    public void after() {
+        memberRepository.deleteAll();
+        alarmRepository.deleteAll();
+    }
+
+    @DisplayName("Consumer는 메시지를 소비하여 알람을 저장한다.")
+    @Test
+    public void handler_ifMember_saveAlarm() throws JsonProcessingException {
+        Member member = Member.builder()
+                .email("member@email")
+                .name("member")
+                .role(Role.VISITOR)
+                .build();
+        memberRepository.save(member);
+        AlarmEvent alarmEvent = AlarmEvent.builder()
+                .email(member.getEmail())
+                .reservationId(1L)
+                .facilityName("test facility")
+                .alarmType(AlarmType.NEW_RESERVATION)
+                .build();
+        String message = objectMapper.writeValueAsString(alarmEvent);
+
+        alarmConsumer.handler(message);
+
+        List<Alarm> retAlarms = alarmRepository.findAll();
+        assertThat(retAlarms).hasSize(1);
+        assertThat(retAlarms).extracting("facilityName")
+                .containsExactly("test facility");
+    }
+
+    @DisplayName("Consumer는 가입되지 않은 멤버의 메시지를 소비할 경우 Exception을 던진다.")
+    @Test
+    public void handler_ifNotMember_throwException() throws JsonProcessingException {
+        AlarmEvent alarmEvent = AlarmEvent.builder()
+                .email("notMember@email.com")
+                .reservationId(1L)
+                .facilityName("test facility")
+                .alarmType(AlarmType.NEW_RESERVATION)
+                .build();
+        String message = objectMapper.writeValueAsString(alarmEvent);
+
+        assertThatThrownBy(() -> alarmConsumer.handler(message))
+                .isInstanceOf(NotFoundEntityException.class);
+    }
+}
---
+++ src/test/java/com/modoospace/alarm/producer/AlarmProducerTest.java
@@ -0,0 +1,78 @@
+package com.modoospace.alarm.producer;
+
+import com.modoospace.AbstractIntegrationContainerBaseTest;
+import com.modoospace.alarm.controller.dto.AlarmEvent;
+import com.modoospace.alarm.domain.Alarm;
+import com.modoospace.alarm.domain.AlarmRepository;
+import com.modoospace.alarm.domain.AlarmType;
+import com.modoospace.member.domain.Member;
+import com.modoospace.member.domain.MemberRepository;
+import com.modoospace.member.domain.Role;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AlarmProducerTest extends AbstractIntegrationContainerBaseTest {
+
+    @Autowired
+    AlarmProducer alarmProducer;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    AlarmRepository alarmRepository;
+
+    @AfterEach
+    public void after() {
+        memberRepository.deleteAll();
+        alarmRepository.deleteAll();
+    }
+
+    @DisplayName("Producer가 메세지를 발행하면 Consumer가 해당 메시지를 소비하여 알람을 저장한다.")
+    @Test
+    public void send_ifMember_saveAlarm() throws InterruptedException {
+        Member member = Member.builder()
+                .email("member@email")
+                .name("member")
+                .role(Role.VISITOR)
+                .build();
+        memberRepository.save(member);
+        AlarmEvent alarmEvent = AlarmEvent.builder()
+                .email(member.getEmail())
+                .reservationId(1L)
+                .facilityName("test facility")
+                .alarmType(AlarmType.NEW_RESERVATION)
+                .build();
+        alarmProducer.send(alarmEvent);
+
+        Thread.sleep(1000);
+
+        List<Alarm> retAlarms = alarmRepository.findAll();
+        assertThat(retAlarms).hasSize(1);
+        assertThat(retAlarms).extracting("facilityName")
+                .containsExactly("test facility");
+    }
+
+    @DisplayName("Producer가 가입되지 않은 이메일을 포함한 메세지를 발행하면, Consumer가 해당 메시지를 소비한후 알람을 저장하지 않는다.")
+    @Test
+    public void send_ifNotMember_notSaveAlarm() throws InterruptedException {
+        AlarmEvent alarmEvent = AlarmEvent.builder()
+                .email("notMember@email.com")
+                .reservationId(1L)
+                .facilityName("test facility")
+                .alarmType(AlarmType.NEW_RESERVATION)
+                .build();
+        alarmProducer.send(alarmEvent);
+
+        Thread.sleep(1000);
+
+        List<Alarm> retAlarms = alarmRepository.findAll();
+        assertThat(retAlarms).hasSize(0);
+    }
+}
