c628b204180b9b86a531f526fe5f5957c9eec96e
hoa0217 <gjwjdghk123@naver.com>
2024-04-01 23:35:02 +0900

    refactor: 예약검색 리팩토링 (공통 request 및 공통 검색 쿼리 도출)
    - 문제: QueryDsl은 객체그래프가 4개이상 넘어가면 NPE가 발생한다.
      hostId 비교시, `reservation.facility.space.host.id`로 접근하여 host에 NPE가 발생함
    - 해결: inner join
    - 출처 : https://www.sunny-son.space/spring/QueryDsl%EC%9D%98%20NPE%20%ED%95%B4%EA%B2%B0%ED%95%98%EA%B8%B0%20+%20Cross%20Join/
    
    Related: #68

--- src/main/java/com/modoospace/reservation/domain/ReservationRepository.java
+++ src/main/java/com/modoospace/reservation/domain/ReservationRepository.java
@@ -1,12 +1,7 @@
 package com.modoospace.reservation.domain;
 
-import com.modoospace.member.domain.Member;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
-    List<Reservation> findByVisitor(Member visitor);
-
-    List<Reservation> findByFacilitySpaceHost(Member host);
 }
