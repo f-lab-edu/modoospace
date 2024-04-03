f6a06b8791603af12713083de72a697cf409e8ac
hoa0217 <gjwjdghk123@naver.com>
2024-04-04 02:14:47 +0900

    refactor: RabbitMQ테스트컨테이너 생성시, reservation과 dlx exchange 및 queue를 생성한다.
    - DLX(Dead Letter Exchange): 소비 시 예외를 던지면 해당 메세지를 거부하고 Queue Flag를 false로 설정하여 DLX로 보낸다.
    
    Related: #70

--- src/test/java/com/modoospace/RabbitMQTestInitializer.java
+++ src/test/java/com/modoospace/RabbitMQTestInitializer.java
@@ -1,12 +1,13 @@
 package com.modoospace;
 
+import com.rabbitmq.client.BuiltinExchangeType;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
-import org.testcontainers.containers.RabbitMQContainer;
-
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.concurrent.TimeoutException;
+import org.testcontainers.containers.RabbitMQContainer;
 
 public class RabbitMQTestInitializer {
 
@@ -20,11 +21,23 @@ public class RabbitMQTestInitializer {
         factory.setPassword("guest");
 
         try (Connection connection = factory.newConnection();
-             Channel channel = connection.createChannel()) {
-            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-            System.out.println("Queue '" + QUEUE_NAME + "' created successfully.");
+                Channel channel = connection.createChannel()) {
+            createExchangeAndQueue(channel);
         } catch (IOException | TimeoutException e) {
             throw new RuntimeException("Failed to create RabbitMQ queue", e);
         }
+    }
+
+    static void createExchangeAndQueue(Channel channel) throws IOException {
+        channel.exchangeDeclare("x.reservation", BuiltinExchangeType.FANOUT);
+        channel.exchangeDeclare("x.reservation.dlx", BuiltinExchangeType.FANOUT);
+
+        HashMap<String, Object> argumentsMap = new HashMap<>();
+        argumentsMap.put("x-dead-letter-exchange", "x.reservation.dlx");
+        channel.queueDeclare("q.reservation", false, false, false, argumentsMap);
+        channel.queueBind("q.reservation", "x.reservation", "");
+
+        channel.queueDeclare("q.reservation.dlx", false, false, false, null);
+        channel.queueBind("q.reservation.dlx", "x.reservation.dlx", "");
     }
 }
