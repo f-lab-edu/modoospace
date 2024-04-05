3d63ceadd8901d8fdc6d835e9b8fe533916915d9
hoa0217 <gjwjdghk123@naver.com>
2024-04-05 13:35:46 +0900

    feature: CustomErrorHandler 정의
    - 심각한 에러일 경우 ImmediateAcknowledgeAmqpException 예외를 던지고, 그 외엔 dlx큐로 보내는 AmqpRejectAndDontRequeueException 예외를 던진다.
    - ImmediateAcknowledgeAmqpException: 발생 시 해당 메세지를 즉시 확인 처리하여, 다시 큐에 넣거나 다른 처리를 하지않는다. 즉, 재처리해도 소용없는 심각한 오류일 경우 해당 예외를 던진다.
    - AmqpRejectAndDontRequeueException: 발생 시 해당 메세지를 거부하고 DLX로 보낸다. 그리고 DLX의 재처리 로직으로 설정한 retry 횟수만큼 다시 시도한다.
    
    Related: #70

---
+++ src/main/java/com/modoospace/config/rabbitmq/CustomErrorHandler.java
@@ -0,0 +1,24 @@
+package com.modoospace.config.rabbitmq;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
+import org.springframework.amqp.ImmediateAcknowledgeAmqpException;
+import org.springframework.amqp.rabbit.listener.FatalExceptionStrategy;
+import org.springframework.util.ErrorHandler;
+
+@RequiredArgsConstructor
+public class CustomErrorHandler implements ErrorHandler {
+
+    private final FatalExceptionStrategy exceptionStrategy;
+
+    @Override
+    public void handleError(Throwable t) {
+        if (this.exceptionStrategy.isFatal(t)) {
+            throw new ImmediateAcknowledgeAmqpException(
+                    "Fatal exception encountered. Retry is futile: " + t.getMessage(), t);
+        }
+
+        throw new AmqpRejectAndDontRequeueException(
+                "Retryable exception encountered. Moving to DLX for retries: " + t.getMessage(), t);
+    }
+}
