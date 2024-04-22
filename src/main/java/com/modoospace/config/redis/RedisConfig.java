3ba7711da3cff90aaf59ecba9a6c2bac17e54669
hoa0217 <gjwjdghk123@naver.com>
2024-04-23 01:02:21 +0900

    chore: Redis Pub/Sub을 위한 Listener(구독자) Config 설정
    - RedisMessageListenerContainer 설정
    
    Related: #73

--- src/main/java/com/modoospace/config/redis/RedisConfig.java
+++ src/main/java/com/modoospace/config/redis/RedisConfig.java
@@ -1,26 +1,36 @@
 package com.modoospace.config.redis;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 
 @Configuration
 public class RedisConfig {
 
-  @Value("${spring.redis.host}")
-  private String host;
-  @Value("${spring.redis.port}")
-  private int port;
-  @Value("${spring.redis.password}")
-  private String password;
+    @Value("${spring.redis.host}")
+    private String host;
+    @Value("${spring.redis.port}")
+    private int port;
+    @Value("${spring.redis.password}")
+    private String password;
 
-  @Bean
-  public RedisConnectionFactory redisConnectionFactory() {
-    RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(host, port);
-    configuration.setPassword(password);
-    return new LettuceConnectionFactory(configuration);
-  }
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(host, port);
+        configuration.setPassword(password);
+        return new LettuceConnectionFactory(configuration);
+    }
+
+    @Bean
+    public RedisMessageListenerContainer redisMessageListenerContainer(
+            @Autowired RedisConnectionFactory redisConnectionFactory) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(redisConnectionFactory);
+        return container;
+    }
 }
